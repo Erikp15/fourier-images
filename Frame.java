3e881b5f8aa6fa4fa98d9d340b765e092fd040ff
Erikp15 <56543366+Erikp15@users.noreply.github.com>
2023-01-03 18:46:26 +0200

    bugged

--- Frame.java
+++ Frame.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package plottest;
+package fourier_series;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -12,19 +12,26 @@ import java.awt.geom.*;
 
 //Extends JPanel class  
 public class Frame extends JFrame implements ActionListener {
-    public static int vectors = 4;
+    public static int vectors = 0;
     public static int fps = 240;
 
     public static void main(String args[]) {
         JFrame frame = new JFrame();
-        complex a = new complex(1, 0);
+        complex a = new complex(1, 1);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         plot p = new plot();
-        for (int i = 1; i <= vectors; i++) {
-            p.addpoint(a.mult(new complex(1 / (double) (i), 0)).add(p.getCord().get(i - 1)), 1 / (double) (i));
-            System.out.println(p.getCord().get(i));
+        p.addpoint(a, 1);
+        for (int i = 2; i <= vectors; i++) {
+            p.addpoint(a.add(p.getCord().get(i - 1)), 1);
         }
-
+        for (int i = vectors + 1; i <= 2 * vectors + 1; i++) {
+            p.addpoint(a.add(p.getCord().get(i - 1)), 1);
+        }        
+        /*
+        for (int i = 1; i <= vectors; i++) {
+            p.addpoint(a.add(p.getCord().get(i - 1)), 1);
+        }
+        */
         frame.add(p);
         frame.setSize(1000, 1000);
         frame.setVisible(true);
@@ -35,12 +42,21 @@ public class Frame extends JFrame implements ActionListener {
                 Thread.currentThread().interrupt();
             }
             // number of full rotations per second
-            double rotations = 1;
-            for (int i = 1; i <= vectors; i++) {
-                p.replacepoint(
-                        rotationcalc(time, rotations * i, p.getCoef().get(i)).add(p.getCord().get(i - 1)),
-                        i);
+            double rotations = 0.1;
+            for (int i = 2; i <= vectors; i++) {
+                p.replacepoint(rotationcalc(time, rotations * (i-1), p.getCoef().get(i)).add(p.getCord().get(i - 1)), i);
             }
+            for (int i = vectors + 1; i <= 2 * vectors + 1; i++) {
+                p.replacepoint(rotationcalc(time, rotations * -(i-vectors), p.getCoef().get(i)).add(p.getCord().get(i - 1)), i);
+            }            
+            /*
+            for (int i = vectors + 1; i <= 2*vectors + 1; i++) {
+                p.replacepoint(
+                        rotationcalc(time, rotations * -(i-vectors), p.getCoef().get(i)).add(p.getCord().get(i)),
+                        1);
+            } 
+*/
+            
             frame.revalidate();
             frame.repaint();
         }
@@ -48,8 +64,8 @@ public class Frame extends JFrame implements ActionListener {
 
     // calculates the rotation of each vector
     public static complex rotationcalc(int time, double rotations, double scaling) {
-        return new complex(Math.cos((time * Math.PI * 2 / fps) * rotations) * scaling,
-                Math.sin((time * Math.PI * 2 / fps) * rotations) * scaling);
+        return new complex(Math.cos(time * Math.PI * 2 / fps * rotations) * scaling,
+                Math.sin(time * Math.PI * 2 / fps * rotations) * scaling);
     }
 
     @Override
