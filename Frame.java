20ca3ce31c30b721e2d4b2b8d6e0b81fe5d93b5f
Erikp15 <56543366+Erikp15@users.noreply.github.com>
2023-01-03 12:53:37 +0200

    Added frame rate parameter

--- Frame.java
+++ Frame.java
@@ -12,15 +12,17 @@ import java.awt.geom.*;
 
 //Extends JPanel class  
 public class Frame extends JFrame implements ActionListener {
+    public static int vectors = 4;
+    public static int fps = 240;
 
     public static void main(String args[]) {
         JFrame frame = new JFrame();
         complex a = new complex(1, 0);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        int vectors = 10;
         plot p = new plot();
         for (int i = 1; i <= vectors; i++) {
-            p.addpoint(a.add(p.getCord().get(i - 1)), 1 / (double) i);
+            p.addpoint(a.mult(new complex(1 / (double) (i), 0)).add(p.getCord().get(i - 1)), 1 / (double) (i));
+            System.out.println(p.getCord().get(i));
         }
 
         frame.add(p);
@@ -28,15 +30,16 @@ public class Frame extends JFrame implements ActionListener {
         frame.setVisible(true);
         for (int time = 0; true; time++) {
             try {
-                Thread.sleep(17);
+                Thread.sleep(1000 / fps);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
             // number of full rotations per second
-            double rotations = 0.5;
+            double rotations = 1;
             for (int i = 1; i <= vectors; i++) {
                 p.replacepoint(
-                        rotationcalc(time, rotations * i, p.getCoef().get(i)).add(p.getCord().get(i - 1)), i);
+                        rotationcalc(time, rotations * i, p.getCoef().get(i)).add(p.getCord().get(i - 1)),
+                        i);
             }
             frame.revalidate();
             frame.repaint();
@@ -45,8 +48,8 @@ public class Frame extends JFrame implements ActionListener {
 
     // calculates the rotation of each vector
     public static complex rotationcalc(int time, double rotations, double scaling) {
-        return new complex(Math.cos((time * Math.PI / 30) * rotations) * scaling,
-                Math.sin((time * Math.PI / 30) * rotations) * scaling);
+        return new complex(Math.cos((time * Math.PI * 2 / fps) * rotations) * scaling,
+                Math.sin((time * Math.PI * 2 / fps) * rotations) * scaling);
     }
 
     @Override
